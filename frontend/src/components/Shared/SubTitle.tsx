ada1cfe0af1b4de2a2155d7d4aa203cd75e38a19
mohamedelmougy <mohamed.elmougy11@gmail.com>
2025-08-26 15:59:24 +0300

    fix(ui): change subtitle direction to LTR

--- frontend/src/components/Shared/SubTitle.tsx
+++ frontend/src/components/Shared/SubTitle.tsx
@@ -6,12 +6,13 @@ interface SubTitleProps {
 const SubTitle = ({ title, btnTitle }: SubTitleProps) => {
   return (
     <div className="container w-full flex items-center justify-between">
+      <span className="font-bold text-2xl text-[#272727]">{title}</span>
       {btnTitle && (
         <button className="border border-[#272727] px-6 py-1.5 rounded-2xl text-[#272727] hover:text-[#f9f9f9] hover:bg-[#272727] cursor-pointer">
           More
         </button>
       )}
-      <span className="font-bold text-2xl text-[#272727]">{title}</span>
+      
     </div>
   )
 }
