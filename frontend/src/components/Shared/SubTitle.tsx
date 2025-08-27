c91e11df9ab6d77382b8142bacfea569736794af
mohamedelmougy <mohamed.elmougy11@gmail.com>
2025-08-27 21:12:30 +0300

    fix(ui): bugfix/replace static display name with prop

--- frontend/src/components/Shared/SubTitle.tsx
+++ frontend/src/components/Shared/SubTitle.tsx
@@ -9,10 +9,9 @@ const SubTitle = ({ title, btnTitle }: SubTitleProps) => {
       <span className="font-bold text-2xl text-[#272727]">{title}</span>
       {btnTitle && (
         <button className="border border-[#272727] px-6 py-1.5 rounded-2xl text-[#272727] hover:text-[#f9f9f9] hover:bg-[#272727] cursor-pointer">
-          More
+          {btnTitle}
         </button>
       )}
-      
     </div>
   )
 }
