b27cf70fe48e61bbd36419633cc848addedc308f
mohamedelmougy <mohamed.elmougy11@gmail.com>
2025-08-27 20:40:00 +0300

    feat(ui): Finalize footer design
    - Implement responsive styling for the footer
    - Add SVG icons as components to support dynamic color and size control

--- frontend/src/assets/icons/index.ts
+++ frontend/src/assets/icons/index.ts
@@ -2,5 +2,9 @@ import user from './user.png'
 import cart from './shopping-cart.png'
 import burgerMenu from './burger-menu.svg'
 import closeMenu from './close-menu.svg'
+import FacebookIcon from './facebook'
+import InstagramIcon from './instagram'
+import XIcon from './x'
 
-export { user, cart, burgerMenu, closeMenu }
+
+export { user, cart, burgerMenu, closeMenu, FacebookIcon, XIcon, InstagramIcon }
--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router'
 import Navbar from '../components/Shared/Navbar'
+import Footer from '@/components/Shared/Footer'
 
 const RootLayout = () => {
   return (
-    <main>
+    <main className='bg-[#f9f9f9] font-lato'>
       <Navbar />
       <Outlet />
+      <Footer />
     </main>
   )
 }
