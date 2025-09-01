10696a86328f2cbc1dbffba31a273d97a4b902b1
mohamedelmougy <mohamed.elmougy11@gmail.com>
2025-09-02 00:06:23 +0300

    feat: replace static link with dynamic prop

--- frontend/src/components/Shared/SubTitle.tsx
+++ frontend/src/components/Shared/SubTitle.tsx
@@ -1,16 +1,22 @@
+import { Link } from 'react-router-dom'
+
 interface SubTitleProps {
   title: string
   btnTitle?: string
+  to: string
 }
 
-const SubTitle = ({ title, btnTitle }: SubTitleProps) => {
+const SubTitle = ({ title, btnTitle, to }: SubTitleProps) => {
   return (
     <div className="container w-full flex items-center justify-between">
       <span className="font-bold text-2xl text-[#272727]">{title}</span>
       {btnTitle && (
-        <button className="border border-[#272727] px-6 py-1.5 rounded-2xl text-[#272727] hover:text-[#f9f9f9] hover:bg-[#272727] cursor-pointer">
+        <Link
+          to={to}
+          className="border border-[#272727] px-6 py-1.5 rounded-2xl text-[#272727] hover:text-[#f9f9f9] hover:bg-[#272727] cursor-pointer"
+        >
           {btnTitle}
-        </button>
+        </Link>
       )}
     </div>
   )
