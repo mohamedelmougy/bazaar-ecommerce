a8c07557e59b7fe7c5fbe06b3c4bcd1565ac878b
mohamedelmougy <mohamed.elmougy11@gmail.com>
2025-09-02 01:55:01 +0300

    fix(ui): remove duplicated height style from brand container

--- frontend/src/components/Brand/BrandContainer.tsx
+++ frontend/src/components/Brand/BrandContainer.tsx
@@ -3,7 +3,7 @@ import BrandCard from './BrandCard'
 
 const BrandContainer = () => {
   return (
-    <div className="container w-full space-y-7 min-h-[75vh]">
+    <div className="container w-full space-y-7">
       <h1 className="text-[#555550] font-extrabold text-2xl">All Brands</h1>
       <div className=" flex items-center justify-center md:justify-between flex-wrap">
         {brands.map((brand) => (
